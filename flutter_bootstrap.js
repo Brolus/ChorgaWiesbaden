6b6bddf1b0e43bf77142d9db57f606fd0ce66c90
Ole Pahlkoetter <ole.pahlkoetter@t-online.de>
2026-03-26 17:51:12 +0100

    Deploy Chorga web from 77ddfd4e386766820b71f074ed192943c2ff47eb

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"e4b8dca3f1b4ede4c30371002441c88c12187e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2708227030" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1648148090" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
